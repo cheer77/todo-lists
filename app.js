0f39b500d469a3d3bca84e274dec96579628f0cc
Oleksandr Chervonniy <sheph@ukr.net>
2023-06-05 15:35:56 +0200

    added function for status change

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ function printTodo({ id, userId, title, completed }) {
 	const status = document.createElement('input');
 	status.type = 'checkbox';
 	status.checked = completed;
+	status.addEventListener('change', handleTodoChange);
 
 	const close = document.createElement('div');
 	close.innerHTML = '&times';
@@ -64,6 +65,13 @@ function handleSubmit(event) {
 	});
 }
 
+function handleTodoChange() {
+	const todoId = this.parentElement.dataset.id;
+	const completed = this.checked;
+
+	toggleTodoComplete(todoId, completed);
+}
+
 //Async logic - функции ассанхронной логики
 async function getAllTodos() {
 	const response = await fetch('https://jsonplaceholder.typicode.com/todos');
@@ -90,4 +98,18 @@ async function createTodo(todo) {
 	const newTodo = await response.json();
 
 	printTodo(newTodo);
+}
+
+async function toggleTodoComplete(todoId, completed) {
+	const response = await fetch(`https://jsonplaceholder.typicode.com/todos/${todoId}`, {
+		method: 'PATCH',
+		body: JSON.stringify({ completed: completed }),
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	});
+
+	if (!response.ok) {
+		//Error
+	}
 }
