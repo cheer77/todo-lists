7756888ab3c3668545ff0a62d072ef6c7604a3da
Oleksandr Chervonniy <sheph@ukr.net>
2023-06-05 14:07:48 +0200

    added list render function

--- app.js
+++ app.js
@@ -1,9 +1,36 @@
 // Globals
+const todoList = document.getElementById('todo-list');
 let todos = [];
 let users = [];
 
-// Attach logic - привязка события
+// Attach Events - привязка события
 document.addEventListener('DOMContentLoaded', initApp);
+
+// Basic Logic
+function getUserName(userId) {
+	const user = users.find((u) => u.id === userId);
+	return user.name;
+}
+
+function printTodo({ id, userId, title, completed }) {
+	const li = document.createElement('li');
+	li.className = 'todo-item';
+	li.dataset.id = id;
+	li.innerHTML = `<span>${title} <i>by</i> <b>${getUserName(userId)}</b> </span>`;
+
+	const status = document.createElement('input');
+	status.type = 'checkbox';
+	status.checked = completed;
+
+	const close = document.createElement('span');
+	close.innerHTML = '&times';
+	close.className = 'close';
+
+	li.prepend(status);
+	li.append(close);
+
+	todoList.prepend(li);
+}
 
 // Event logic - логига события подгрузки страницы
 function initApp() {
@@ -11,6 +38,7 @@ function initApp() {
 		[todos, users] = values;
 
 		// Отправить в разметку тут
+		todos.forEach((todo) => printTodo(todo));
 	});
 }
 
